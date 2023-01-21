d8861911ff3bb9991c44311328bf554e484c75dd
Jakub Skowron <jakub@skowron.dev>
2023-01-21 18:42:44 +0100

    rich.console

--- main.py
+++ main.py
@@ -5,26 +5,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, chi2, RFE
 from tqdm import tqdm
 import utils
+from rich.console import Console
 
+console = Console()
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    console.print("Packet Predict&Analyze")
+    console.print("Using model: UNSW-NB15")
+
     # Load dataset
-    data = pd.read_csv("UNSW-NB15/trening/UNSW_NB15_training-set.csv", dtype=utils.generate_dtype(), low_memory=False)
-    # # Data preprocessing
+    with console.status("# Reading csv..."):
+        data = pd.read_csv("trening_data/UNSW_NB15_training-set.csv", dtype=utils.generate_dtype())
+
+    console.print("# Data preprocessing...")
     data = data.dropna()  # remove missing values
     data = data.drop("id", axis=1)  # remove id column
 
-    # # Split dataset into features and labels
+    console.print("# Split dataset into features and labels...")
     X = data.drop(['label', 'attack_cat', 'proto', 'service', 'state'], axis=1)
     y = data['label'].factorize()[0]
 
-    # print(X)
-    # print(y)
-
     # # Split dataset into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    #
+
     # Create and train the model
     clf = RandomForestClassifier()
     selector = RFE(clf, n_features_to_select=10)
@@ -36,11 +39,11 @@ if __name__ == '__main__':
     # Get the selected features
     X_new = selector.transform(X_train)
 
-    # Create and train the model
-    clf = RandomForestClassifier()
-    clf.fit(X_new, y_train)
+    console.log("# Create and train the model with selected features...")
+    for _ in tqdm(range(len(X_new))):
+        clf.fit(X_new, y_train)
 
-    # Start capturing packets
+    console.log("# Start capturing packets....")
     capture = pyshark.LiveCapture(interface='en0')
     capture.sniff(timeout=50)
 
@@ -55,6 +58,6 @@ if __name__ == '__main__':
 
             # Extract features of the packet
             packet_features = [packet.ip.src, packet.tcp.srcport, packet.ip.dst, packet.tcp.dstport]
-            # Predict the probability of vulnerability
+            console.log("# Predict the probability of vulnerability")
             proba = clf.predict_proba([packet_features])
             print("Probability of vulnerability:", proba)
